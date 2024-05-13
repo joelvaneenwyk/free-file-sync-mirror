f491fddd0aef86e881f633a12bfd5b108b8b9a04
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-13 15:33:36 -0700

    Script updates to ensure zero errors

--- .github/build/build-wxwidgets.sh
+++ .github/build/build-wxwidgets.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-set -eux
-wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.3/wxWidgets-3.2.3.tar.bz2
-tar xvf wxWidgets-3.2.3.tar.bz2
-cd wxWidgets-3.2.3/ || exit
-mkdir gtk-build
+set -eaux
+VERSION="${VERSION:-${1:-3.2.3}}"
+wget "https://github.com/wxWidgets/wxWidgets/releases/download/v$VERSION/wxWidgets-$VERSION.tar.bz2"
+tar xvf "wxWidgets-$VERSION.tar.bz2"
+cd "wxWidgets-$VERSION/" || exit
+mkdir -p gtk-build
 cd gtk-build/ || exit
 ../configure --disable-shared --enable-unicode --enable-no_exceptions
 make -j "$(nproc)"
--- .github/build/build.sh
+++ .github/build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eux
+set -eaux
 ./build-gcc.sh
 ./build-curl.sh
 ./build-openssl.sh
