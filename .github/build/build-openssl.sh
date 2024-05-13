6f68076dcf9104eabb972250a1fc048c9dd68106
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-13 15:14:58 -0700

    Create build scripts

---
+++ .github/build/build-openssl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget https://www.openssl.org/source/openssl-3.0.0.tar.gz
+tar xvf openssl-3.0.0.tar.gz
+cd openssl-3.0.0 || exit
+mkdir -p build
+cd build || exit
+../config
+make -j 8
+sudo make install
+sudo ldconfig
---
+++ .github/build/build-wxwidgets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.3/wxWidgets-3.2.3.tar.bz2
+tar xvf wxWidgets-3.2.3.tar.bz2
+cd wxWidgets-3.2.3/ || exit
+mkdir gtk-build
+cd gtk-build/ || exit
+../configure --disable-shared --enable-unicode --enable-no_exceptions
+make -j 8
+sudo make install
