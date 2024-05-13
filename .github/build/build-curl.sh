be9d07aec874a0cfaa0551deed3faeef041ccd7e
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-13 15:14:58 -0700

    Create build scripts

---
+++ .github/build/build-curl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://curl.se/download/curl-8.4.0.tar.gz
+tar xvf curl-8.4.0.tar.gz
+cd curl-8.4.0/ || exit
+mkdir -p build
+cd build/ || exit
+../configure --with-openssl --with-libssh2 --enable-versioned-symbols
+make -j 8
+sudo make install
---
+++ .github/build/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./build-curl.sh
+./build-openssl.sh
+./build-wxwidgets.sh
+./build-gcc.sh
