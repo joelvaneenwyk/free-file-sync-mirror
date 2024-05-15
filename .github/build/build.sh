8d151814bf7b192a03dbcb67532619ad2bc06cd5
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-14 17:00:39 -0700

    add taskfile

--- .github/build/build.sh
+++ .github/build/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Use BASH_SOURCE to get the current directory of the script
+REPO_ROOT="$(dirname "${BASH_SOURCE[0]}")"
+
 set -eaux
-./build-gcc.sh
-./build-curl.sh
-./build-openssl.sh
-./build-wxwidgets.sh
+"$REPO_ROOT/build-gcc.sh"
+"$REPO_ROOT/build-curl.sh"
+"$REPO_ROOT/build-openssl.sh"
+"$REPO_ROOT/build-wxwidgets.sh"
