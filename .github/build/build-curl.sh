ac9971b62ab7c42b6423fbebea9d901b6cc08135
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-08-16 19:06:20 -0700

    Update scripts to be slightly more reentrant
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- .github/build/build-curl.sh
+++ .github/build/build-curl.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 set -eaux
-VERSION="${VERSION:-${1:-8.4.0}}"
-wget "https://curl.se/download/curl-$VERSION.tar.gz"
-tar xvf "curl-$VERSION.tar.gz"
-cd "curl-$VERSION/" || exit
+CURL_VERSION="${CURL_VERSION:-${1:-8.9.1}}"
+REPO_ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && cd ../../ && pwd)
+BUILD_DIR="$REPO_ROOT/.build"
+FILENAME="curl-$CURL_VERSION.tar.gz"
+if [ ! -e "$BUILD_DIR/$FILENAME" ]; then
+    wget \
+        "https://curl.se/download/$FILENAME" \
+        -O "$BUILD_DIR/$FILENAME"
+fi
+tar xvf "$BUILD_DIR/$FILENAME" -C "$BUILD_DIR"
+cd "$BUILD_DIR/curl-$CURL_VERSION/" || exit
 mkdir -p build
 cd build/ || exit
 ../configure --with-openssl --with-libssh2 --enable-versioned-symbols
--- .github/build/build.sh
+++ .github/build/build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Use BASH_SOURCE to get the current directory of the script
-REPO_ROOT="$(dirname "${BASH_SOURCE[0]}")"
-
 set -eaux
 
-# shellcheck source=./build-gcc.sh
-source "$REPO_ROOT/build-gcc.sh"
+# Use BASH_SOURCE to get the current directory of the script
+REPO_ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && cd ../../ && pwd)
 
-# shellcheck source=./build-curl.sh
-source "$REPO_ROOT/build-curl.sh"
+# shellcheck source=./build-gcc.sh
+source "$REPO_ROOT/.github/build/build-gcc.sh"
 
 # shellcheck source=./build-openssl.sh
-source "$REPO_ROOT/build-openssl.sh"
+source "$REPO_ROOT/.github/build/build-openssl.sh"
+
+# Curl depends on OpenSSL hence why this comes after 'build-openssl.sh'
+# shellcheck source=./build-curl.sh
+source "$REPO_ROOT/.github/build/build-curl.sh"
 
 # shellcheck source=./build-wxwidgets.sh
-source "$REPO_ROOT/build-wxwidgets.sh"
+source "$REPO_ROOT/.github/build/build-wxwidgets.sh"
