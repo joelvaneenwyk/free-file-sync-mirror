cb166581bed2b50b485ae2e105b18d81e1ffa16b
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-09-28 15:15:38 -0700

    GCC and wxWidgets work
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- .github/build/build-wxwidgets.sh
+++ .github/build/build-wxwidgets.sh
@@ -4,22 +4,27 @@ set -eaux
 WXWIDGETS_VERSION="${WXWIDGETS_VERSION:-${1:-3.2.3}}"
 REPO_ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && cd ../../ && pwd)"
 BUILD_DIR="$REPO_ROOT/.build"
+ARCHIVE_DIR="$REPO_ROOT/.build/archives"
 INSTALL_DIR="$REPO_ROOT/.build/lib"
+SOURCE_DIR="$REPO_ROOT/.build/src"
 FILENAME="wxWidgets-$WXWIDGETS_VERSION.tar.bz2"
-if [ ! -e "$BUILD_DIR/$FILENAME" ]; then
+if [ ! -e "$ARCHIVE_DIR/$FILENAME" ]; then
   # e.g., https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.3/wxWidgets-3.2.3.tar.bz2
-  mkdir -p "$BUILD_DIR"
+  mkdir -p "$ARCHIVE_DIR"
   wget \
     "https://github.com/wxWidgets/wxWidgets/releases/download/v$WXWIDGETS_VERSION/$FILENAME" \
-    -O "$BUILD_DIR/$FILENAME"
+    -O "$ARCHIVE_DIR/$FILENAME"
 fi
-tar xf "$BUILD_DIR/$FILENAME" -C "$BUILD_DIR"
-TARGET_DIR="$BUILD_DIR/wxWidgets-$WXWIDGETS_VERSION"
+TARGET_DIR="$SOURCE_DIR/wxWidgets-$WXWIDGETS_VERSION"
+if [ ! -d "$TARGET_DIR" ]; then
+  tar xf "$ARCHIVE_DIR/$FILENAME" -C "$SOURCE_DIR"
+fi
 TARGET_BUILD_DIR="$BUILD_DIR/tmp/wx"
 mkdir -p "$TARGET_BUILD_DIR" "$INSTALL_DIR"
 cd "$TARGET_BUILD_DIR" || exit
 if [ ! -e "$TARGET_BUILD_DIR/Makefile" ]; then
-  "$TARGET_DIR/configure" \
+  CONFIG="$(realpath -s --relative-to="$TARGET_BUILD_DIR" "$TARGET_DIR/configure")"
+  "$CONFIG" \
     --prefix="$INSTALL_DIR" \
     --disable-shared \
     --enable-unicode \
