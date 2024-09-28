ea6f3e1481a9b796893b88f76334799238a38bc9
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-09-27 19:56:36 -0700

    Shell script cleanup
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- .github/build/build-curl.sh
+++ .github/build/build-curl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -eaux
-CURL_VERSION="${CURL_VERSION:-${1:-8.9.1}}"
-REPO_ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && cd ../../ && pwd)
+CURL_VERSION="${CURL_VERSION:-${1:-8.10.1}}"
+REPO_ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && cd ../../ && pwd)"
 BUILD_DIR="$REPO_ROOT/.build"
 FILENAME="curl-$CURL_VERSION.tar.gz"
 if [ ! -e "$BUILD_DIR/$FILENAME" ]; then
@@ -11,9 +11,12 @@ if [ ! -e "$BUILD_DIR/$FILENAME" ]; then
     -O "$BUILD_DIR/$FILENAME"
 fi
 tar xvf "$BUILD_DIR/$FILENAME" -C "$BUILD_DIR"
-cd "$BUILD_DIR/curl-$CURL_VERSION/" || exit
-mkdir -p build
-cd build/ || exit
-../configure --with-openssl --with-libssh2 --enable-versioned-symbols
+TARGET_DIR="$BUILD_DIR/curl-$CURL_VERSION"
+mkdir -p "$TARGET_DIR/build" "$BUILD_DIR/install"
+cd "$TARGET_DIR/build" || exit
+if [ ! -e "$TARGET_DIR/build/Makefile" ]; then
+  "$TARGET_DIR/configure" --prefix="$BUILD_DIR/install" --with-openssl --with-libssh2 --enable-versioned-symbols
+fi
 make -j "$(nproc)"
-sudo make install
+
+make install
--- .github/build/build-wxwidgets.sh
+++ .github/build/build-wxwidgets.sh
@@ -2,18 +2,25 @@
 
 set -eaux
 WXWIDGETS_VERSION="${WXWIDGETS_VERSION:-${1:-3.2.3}}"
-REPO_ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && cd ../../ && pwd)
+REPO_ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && cd ../../ && pwd)"
 BUILD_DIR="$REPO_ROOT/.build"
-FILENAME="wxWidgets-$WXWIDGETS_VERSION.tar.gz"
+FILENAME="wxWidgets-$WXWIDGETS_VERSION.tar.bz2"
 if [ ! -e "$BUILD_DIR/$FILENAME" ]; then
+  # e.g., https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.3/wxWidgets-3.2.3.tar.bz2
   wget \
-    "https://github.com/wxWidgets/wxWidgets/releases/download/v$WXWIDGETS_VERSION/wxWidgets-$WXWIDGETS_VERSION.tar.bz2" \
+    "https://github.com/wxWidgets/wxWidgets/releases/download/v$WXWIDGETS_VERSION/$FILENAME" \
     -O "$BUILD_DIR/$FILENAME"
 fi
-tar xvf "wxWidgets-$WXWIDGETS_VERSION.tar.bz2"
-cd "wxWidgets-$WXWIDGETS_VERSION/" || exit
-mkdir -p gtk-build
-cd gtk-build/ || exit
-../configure --disable-shared --enable-unicode --enable-no_exceptions
+tar xvf "$BUILD_DIR/$FILENAME" -C "$BUILD_DIR"
+TARGET_DIR="$BUILD_DIR/wxWidgets-$WXWIDGETS_VERSION"
+mkdir -p "$TARGET_DIR/gtk-build" "$BUILD_DIR/install"
+cd "$TARGET_DIR/gtk-build" || exit
+if [ ! -e "$TARGET_DIR/gtk-build/Makefile" ]; then
+  "$TARGET_DIR/configure" \
+    --disable-shared \
+    --enable-unicode \
+    --enable-no_exceptions \
+    --prefix="$BUILD_DIR/install"
+fi
 make -j "$(nproc)"
-# sudo make install
+make install
