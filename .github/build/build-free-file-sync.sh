84d64fc378621964ef863fa60ff90b838ee841fd
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-09-29 23:11:38 -0700

    Build script updates for FreeFileSync
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- .github/build/build-free-file-sync.sh
+++ .github/build/build-free-file-sync.sh
@@ -10,10 +10,13 @@ CURL_VERSION="${CURL_VERSION:-${1:-8.10.1}}"
 REPO_ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && cd ../../ && pwd)"
 BUILD_DIR="$REPO_ROOT/.build"
 INSTALL_DIR="$REPO_ROOT/.build/lib"
-SOURCE_DIR="$REPO_ROOT/FreeFileSync/Source"
-PATH="$BUILD_DIR/lib/bin:${PATH:-}"
 TARGET_BUILD_DIR="$BUILD_DIR/tmp/freefilesync"
 mkdir -p "$TARGET_BUILD_DIR" "$INSTALL_DIR"
-cd "$SOURCE_DIR" || exit
-make -j "$(nproc)" all
+
+PATH="$BUILD_DIR/lib/bin:${PATH:-}"
+
+SOURCE_DIR="$REPO_ROOT/FreeFileSync/Source"
+SOURCE_DIR_REL="$(realpath -s --relative-to="$TARGET_BUILD_DIR" "$SOURCE_DIR")"
+cd "$TARGET_BUILD_DIR" || exit
+make -C "$SOURCE_DIR_REL" -j "$(nproc)" all
 # make -C "$REPO_ROOT/FreeFileSync/Source" install
